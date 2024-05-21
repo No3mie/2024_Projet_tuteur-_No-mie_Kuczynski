56c9bd4c3fd2255eb5f83c30245da1b0f1983891
No3mie <167068483+No3mie@users.noreply.github.com>
2024-05-21 16:37:16 +0200

    Update analyse_data_Fr_Ch_Oak.R

--- analyse_data_Fr_Ch_Oak.R
+++ analyse_data_Fr_Ch_Oak.R
@@ -33,5 +33,60 @@ barplot(Donnes_China_M_France_2001_2020$primaryValue,
         main = "Importations du bois ronds de Chênes français vers la Chine", sub = "Entre 2001 et 2020",
         col= "lavender",   col.main="red", col.lab="blue", col.sub="black")
 
-# Changer les échelles et faire apparâitre les années 
-# Bien caser les titres sur 2 lignes 
+# Changer les échelles et faire apparaitre les années 
+# Mettre les légendes à la même taille et mettre des couleurs harmonieuses 
+#utiliser ggplot2 
+
+###############################################################################################
+###### Ajouter l'année 2002 aux imports de la Chine 
+nouvelle_ligne <- c("2002",156, "China","M",251,"France","H","","FALSE","", "","" , 0, "","" )
+tableau <- rbind(Donnes_China_M_France_2001_2020, nouvelle_ligne)
+
+################################################################################################
+#### Création d'un nouveau graphique avec les imports et exports
+
+# chargement package
+library(ggplot2)
+library(dplyr)
+library(hrbrthemes)
+
+# Création du data frame
+time <- Donnes_France_X_Chine_2001_2020$Year
+valeurs_imports_chine <- tableau$primaryValue
+valeurs_exports_france <- Donnes_France_X_Chine_2001_2020$legacyEstimationFlag
+# 
+compil_donnees <- data.frame(time, valeurs_imports_chine, valeurs_exports_france)
+# A refaire, les chiffres sont dans le mauvais ordre
+
+# Conversion de la colonne 'temps' en format Date
+compil_donnees$time <- as.Date(compli_donnees$time)
+
+# Créer le graphique barplot
+#ggplot(data, aes(temps, fill = factor(variable))) +
+#  geom_bar(position = "dodge", stat = "identity") +
+#  labs(x = "Temps", y = "Valeurs", fill = "Catégorie") +
+#  ggtitle("Comparaison de deux catégories en fonction du temps")
+
+#####################################################
+####Jointure de table 
+#library('dplyr')
+
+# Methode 1 
+#export = data[(data$flowCode == "X"),]
+#import = data[(data$flowCode == "M"),]
+#mirror_flow = full_join(export[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")], 
+                    #    import[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")], 
+                    #   by=c("cmdCode", "period", "reporterDesc"="partnerDesc", "partnerDesc"="reporterDesc"))
+
+#Methode 2 
+#mirror_flow = merge(x= export[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
+               #     y= import[c("cmdCode","period","reporterDesc","partnerDesc","primaryValue")],
+                #    by.x=c("cmdCode", "period", "reporterDesc", "partnerDesc"),
+                 #   by.y=c("cmdCode", "period", "partnerDesc", "reporterDesc"),
+                  #  all.x = TRUE, all.y = TRUE)
+
+
+#toMatch = c('\\d','XX','_X')
+#data[!(grepl(paste(toMatch,collapse="|"), data$reporterISO)) &
+ #      !(grepl(paste(toMatch,collapse="|"), data$partnerISO)),]
+
