18d7b8ca1cf4bfdfc6562c0dedace7138823fffd
No3mie <167068483+No3mie@users.noreply.github.com>
2024-05-24 19:58:23 +0200

    Create graphiques_data_Fr_Ch_Oak.R

---
+++ graphiques_data_Fr_Ch_Oak.R
@@ -0,0 +1,25 @@
+####################################################################################################################################
+######################################################### CREATION GRAPHIQUES ######################################################
+####################################################################################################################################
+
+# Chemin d'accès
+setwd("C:/Users/Nono/Desktop/Univ Lorraine/Projet TUT/R/2024_Projet_tuteure_Noemie_Kuczynski")
+
+# Ouverture données 
+Data_Fr_Ch_combine_2001_2020 <- read.csv("Data_Fr_Ch_combine_2001_2020.csv", sep = ";", stringsAsFactors = T)
+str(Data_Fr_Ch_combine_2001_2020)
+
+
+#######################################################################################################################################
+# Création d'un graphique avec la chronologie des imports et exports ($)
+
+
+
+
+
+
+
+#######################################################################################################################################
+# Création d'un graphique des proportions des pays qui exportent à la chine 
+
+#Création d'un graphique des proportions des pays à qui la France exporte
