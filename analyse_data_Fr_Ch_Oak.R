521763c24f19a96c23f5b552e90817d06fc2d855
No3mie <167068483+No3mie@users.noreply.github.com>
2024-05-24 10:23:26 +0200

    Update analyse_data_Fr_Ch_Oak.R

--- analyse_data_Fr_Ch_Oak.R
+++ analyse_data_Fr_Ch_Oak.R
@@ -1,27 +1,32 @@
-##############################################################################################################################
-################################################### ANALYSE DES DONNEES ######################################################
-##############################################################################################################################
+####################################################################################################################################
+################################################### TRAITENEMENTS DES DONNEES ######################################################
+####################################################################################################################################
 
 # Chemin d'accès
 setwd("C:/Users/Nono/Desktop/Univ Lorraine/Projet TUT/R/2024_Projet_tuteure_Noemie_Kuczynski")
 
 # Ouverture données 
-library(readr)
-Donnes_France_X_Chine_2001_2020 <- read_delim("Donnes_France_X_Chine_2001_2020.csv", 
-                                              delim = ";", escape_double = FALSE, trim_ws = TRUE)
+#library(readr)
+#Donnes_France_X_Chine_2001_2020 <- read_delim("Donnes_France_X_Chine_2001_2020.csv", 
+                                           #   delim = ";", escape_double = FALSE, trim_ws = TRUE)
+Donnes_France_X_Chine_2001_2020 <- read.csv("Donnes_France_X_Chine_2001_2020.csv", sep = ";", stringsAsFactors = T)
+str(Donnes_France_X_Chine_2001_2020)
 
-Donnes_China_M_France_2001_2020 <- read_delim("Donnes_China_M_France_2001_2020.csv", 
-                                              delim = ";", escape_double = FALSE, trim_ws = TRUE)
-
+#Donnes_China_M_France_2001_2020 <- read_delim("Donnes_China_M_France_2001_2020.csv", 
+ #                                             delim = ";", escape_double = FALSE, trim_ws = TRUE)
+Donnes_China_M_France_2001_2020 <- read.csv("Donnes_China_M_France_2001_2020.csv", sep = ";", stringsAsFactors = T)
+str(Donnes_China_M_France_2001_2020)
 
 #Exploration des donnees 
-str(Donnes_France_X_Chine_2001_2020)
-#legacyEstimationFlag est bien en num
 str(Donnes_China_M_France_2001_2020)
-# primaryValue est num 
+summary(Donnes_China_M_France_2001_2020)
+#legacyEstimationFlag et primaryvalue sont en int mais je peux quand même avec des médiannes, min, max,...
 # Mais il manque 2002 
-Donnes_China_M_France_2001_2020$Year <- as.factor(Donnes_China_M_France_2001_2020$Year)
-str(Donnes_China_M_France_2001_2020)
+
+###############################################
+###### Transformet la colonne des années en facteur
+#Donnes_China_M_France_2001_2020$Year <- as.factor(Donnes_China_M_France_2001_2020$Year)
+#str(Donnes_China_M_France_2001_2020)
 #Les annees sont des factors 
 
 ###############################################
@@ -39,50 +44,78 @@ str(Donnes_China_M_France_2001_2020)
 #Toujours en chr 
 
 # Ajouter une nouvelle ligne avec un élément de type factor
-Ligne_2002_V2 <- data.frame(Year = factor("2002"),
-                            reporterCode = 156,
+library(dplyr)
+Ligne_2002_V2 <- data.frame(Year = as.integer("2002"),
+                            reporterCode = as.integer("156"),
                             reporterName = "China",
                             flowCode = "M",
-                            partenerCode = 251,
+                            partenerCode = as.integer("251"),
                             partenerName = "France",
                             classificationCode = "H",
-                            cifValue = 0,
-                            fobValue = "FALSE",
+                            cifValue = as.integer("0"),
+                            fobValue = as.logical("FALSE") ,
                             primaryValue = 0,
                             legacyEstimationFlag = 0, 
                             isReported = 0,
-                            isAgregate = 0,
-                            NA...14 = "FALSE",
-                            NA...15 = "FALSE")
-
-Donnes_China_M_France_2001_2020_av2002_V3 <- rbind(Donnes_China_M_France_2001_2020_av2002, Ligne_2002_V2)
-library(dplyr)
-
-Donnes_China_M_France_2001_2020_av2002_V3 <- bind_rows(Donnes_China_M_France_2001_2020_av2002, Ligne_2002_V2)
-#Je peux pas combine parce que qu'il y a un character et un douvble pour la colonne reporerCode
+                            isAgregate = as.integer("0"),
+                            NA...14 = as.logical("FALSE"),
+                            NA...15 = as.logical("FALSE") )
+colnames(Ligne_2002_V2) = colnames(Donnes_China_M_France_2001_2020)
+length(rownames(Ligne_2002_V2))
+str(Ligne_2002_V2)
+Donnes_China_M_France_2001_2020_av2002_V3 <- rbind(Donnes_China_M_France_2001_2020, Ligne_2002_V2)
 str(Donnes_China_M_France_2001_2020_av2002_V3)
+#Le nouveau tableau est validé, j'ai bien toutes les années pour l'importation de la Chine
+#int pour Year
+
+#Test barplot 
+barplot(Donnes_China_M_France_2001_2020_av2002_V3$primaryValue~Donnes_China_M_France_2001_2020_av2002_V3$Year)
+#test validé, on peut passer aux choses sérieuses
+
+##############################################################################
+####USELESS 
+#Donnes_China_M_France_2001_2020_av2002_V3 <- bind_rows(Donnes_China_M_France_2001_2020_av2002, Ligne_2002_V2)
+#Je peux pas combine parce que qu'il y a un character et un double pour la colonne reporerCode
+#str(Donnes_China_M_France_2001_2020_av2002_V3)
 
 ###### Ordonner et bien vérifier que les données monétaires soient en num et les années en factor ? 
-as.factor( Donnes_China_M_France_2001_2020_av2002$Year)
-as.numeric(Donnes_China_M_France_2001_2020_av2002$primaryValue)
-str(Donnes_China_M_France_2001_2020)
-str(Donnes_China_M_France_2001_2020_av2002)
+#as.factor( Donnes_China_M_France_2001_2020_av2002$Year)
+#as.numeric(Donnes_China_M_France_2001_2020_av2002$primaryValue)
+#str(Donnes_China_M_France_2001_2020)
+#str(Donnes_China_M_France_2001_2020_av2002)
 
-order(Donnes_China_M_France_2001_2020_av2002$Year)
+#Index <- order(Donnes_China_M_France_2001_2020_av2002_V3$Year)
 # order(Df$nom_colonne1, decreasing=FALSE)
+# Index = liste de l'ordre dans laquelle il faut mettre les lignes de Donnees_China_..._av2002_V3
+#Donnes_China_M_France_2001_2020_av2002_V3 <- data[Index, ]
+########################################################################
+
+# Ordonner les années 
+#library(dplyr)
+# noramelement déjà ouvert 
+Donnes_China_M_France_2001_2020_av2002_V3_order <- arrange(Donnes_China_M_France_2001_2020_av2002_V3, Year)
 
 
-####################################################################
-
-#Index <- order(Donnes_China_M_France_2001_2020_av2002$Year)
-# c'est l'ordre dans laquelle il faut mettre les lignes du Data China av 2002
-#Donnes_China_M_France_2001_2020_av2002 <- data [Index, ]
-
-
+###### Création d'un tableau qui regroupe les deux tables 
+str(Donnes_China_M_France_2001_2020_av2002_V3_order)
+str(Donnes_France_X_Chine_2001_2020)
+Donnes_Fr_Ch_mix_2001_2020 <- bind_rows(Donnes_France_X_Chine_2001_2020, Donnes_China_M_France_2001_2020_av2002_V3_order)
+#nouveau tableau bien créé 
 
 
-##############################################
-##############################################
+# Ordonner les années de ce superbe tableau 
+#library(dplyr)
+# noramelement déjà ouvert 
+Donnes_Fr_Ch_mix_2001_2020_order <- arrange(Donnes_Fr_Ch_mix_2001_2020, Year)
+#c'est cool ça 
+#mais en fait il me faudrait une seule colonne pour les $ pour faire des graphs 
+
+
+
+###################################################################################################################################
+
+#####################################################################
+#####################################################################
 #Création graphiques 
 plot(Donnes_France_X_Chine_2001_2020$legacyEstimationFlag)
 barplot(Donnes_France_X_Chine_2001_2020$legacyEstimationFlag)
@@ -111,7 +144,7 @@ barplot(Donnes_China_M_France_2001_2020$primaryValue,
 
 
 
-#####################################################
+##############################################################################################
 ####Jointure de table 
 #library('dplyr')
 
